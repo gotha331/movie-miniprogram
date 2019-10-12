d70b1c61d41b77036d041d747ae318f4d941e07b
rivers-guitar <530025909@qq.com>
2019-10-12 17:17:42 +0800

    feat: Comment feature

--- miniprogram/pages/comment/comment.js
+++ miniprogram/pages/comment/comment.js
@@ -1,4 +1,9 @@
-// pages/comment/comment.js
+import Notify from 'vant-weapp/notify/notify';
+// import Msg from 'weui-miniprogram/msg/msg';
+
+//初始化云数据库
+const db = wx.cloud.database();
+
 Page({
 
   /**
@@ -6,8 +11,121 @@ Page({
    */
   data: {
     movieid: -1,
-    details: {}
+    details: {},
+    content: '',
+    rate: 0,
+    selectFile: [],
+    uplaodFile: [],
+    temFiles: [],
+    files: [],
+    fileIds: []
   },
+
+  /**
+   * 评价
+   */
+  onContentChange: function(event) {
+    this.setData({
+      content: event.detail
+    });
+  },
+  /**
+   * 评分
+   */
+  onRateChange: function(event) {
+    this.setData({
+      rate: event.detail
+    });
+  },
+
+  previewImage: function(e) {
+    wx.previewImage({
+      current: e.currentTarget.id, // 当前显示图片的http链接
+      urls: this.data.files // 需要预览的图片http链接列表
+    })
+  },
+
+  selectFile: function(files) {
+    this.setData({
+      temFiles: []
+    })
+
+    this.setData({
+      temFiles: this.data.temFiles.concat(files.tempFilePaths)
+    })
+    // 返回false可以阻止某次文件上传
+  },
+
+  /**
+   * 删除照片
+   */
+  deleteFile: function(e) {
+    this.data.files.splice(e.detail.index, 1)
+    this.data.temFiles.splice(e.detail.index, 1)
+    this.setData({
+      files: this.data.files,
+      temFiles: this.data.temFiles
+    })
+  },
+
+  uplaodFile: function(files) {
+    // 文件上传的函数，返回一个promise
+    return new Promise((resolve, reject) => {
+      for (let i = 0; i < this.data.temFiles.length; i++) {
+        let item = this.data.temFiles[i];
+        let suffix = /\.\w+$/.exec(item)[0]; //正则表达式，返回文件扩展名 
+
+        wx.cloud.uploadFile({
+          cloudPath: new Date().getTime() + suffix, //上传至云端的路径
+          filePath: this.data.temFiles[i], // 文件路径
+        }).then(res => {
+          let json = {
+            url: this.data.temFiles[i]
+          }
+          this.setData({
+            files: this.data.files.concat(json),
+            fileIds: this.data.fileIds.concat(res.fileID)
+          })
+
+        }).catch(error => {
+          console.log(error)
+        })
+      }
+    })
+  },
+
+  /**
+   * 提交评价
+   */
+  submitComment: function() {
+    wx.showLoading({
+      title: '评价提交中',
+    })
+
+    db.collection('comment').add({
+      data: {
+        movieid: this.data.movieid,
+        content: this.data.content,
+        rate: this.data.rate,
+        fileIds: this.data.fileIds
+      }
+    }).then(res => {
+      // wx.hideLoading();
+      wx.showToast({
+        title: '提交成功'
+      })
+
+      setTimeout(() => {
+        wx.navigateBack({
+          delta: 1
+        })
+      }, 1000)
+
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
@@ -18,7 +136,9 @@ Page({
     })
 
     this.setData({
-      movieid: Number(options.movieid)
+      movieid: Number(options.movieid),
+      selectFile: this.selectFile.bind(this),
+      uplaodFile: this.uplaodFile.bind(this)
     })
 
     wx.cloud.callFunction({
@@ -27,8 +147,6 @@ Page({
         movieid: this.data.movieid
       }
     }).then(res => {
-
-      console.log(res.result)
       this.setData({
         details: res.result
       })
